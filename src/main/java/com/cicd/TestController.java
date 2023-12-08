2a70d58adcca613de78b7275b88dcbe05b71427b
dgarcia <manuonda@gmail.com>
2023-12-07 23:57:12 -0300

    init project

---
+++ src/main/java/com/cicd/TestController.java
@@ -0,0 +1,16 @@
+package com.cicd;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1")
+public class TestController {
+
+    @GetMapping("/hola")
+    public String getHello() {
+        return "Hola Mundo";
+    }
+}
